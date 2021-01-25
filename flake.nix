cb1fa5de168e372a332200c150f780a20f9bf696
Philipp Herzog <philipp.herzog@protonmail.com>
2021-01-25 07:50:56 +0100

    update lockfile

--- flake.nix
+++ flake.nix
@@ -11,7 +11,6 @@
     neovim-nightly-src = { url = "github:neovim/neovim"; flake = false; };
     # rofi source not here since rofi requires submodules which flake inputs dont support yet
     # rofi-wayland-src = { url = "github:lbonn/rofi"; flake = false; submodules = true; };
-    # rofi-pass-gopass ... phew
     # rofi-pass-gopass-src = { url = "github:carnager/rofi-pass/gopass"; flake = false; };
   };
   outputs = { self, nixpkgs, neovim-nightly-src, home-manager, nixos-hardware, ... }@inputs: let 
@@ -27,6 +26,8 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system overlays; config.allowUnfree = true; };
 
+    # every setup is a system + a user
+    # the system is mainly used for hardware config, the user for software-specific setups
     mkSetup = {name, host, username, extramods ? []}: let 
       hostmod = import (./hosts + "/${host}") { inherit pkgs username; };
       ret = nixpkgs.lib.nixosSystem {
@@ -43,6 +44,9 @@
       setup-script = pkgs.writeShellScriptBin "setup" ''
         if [[ -z "$1" || "$1" == "help" ]]; then
           echo -e "Usage: $(basename $0) {config} [ update | switch | build | install ]\n\nFor more details on the options see \`man nixos-rebuild\`"
+          echo -e ""
+          echo -e "Available configs:"
+          echo -e "   - \"nixos-laptop\":  laptop setup for work"
         elif [[ "$1" == "update" ]]; then 
           nix flake update --recreate-lock-file --commit-lock-file
         elif [[ "$2" == "install" ]]; then
