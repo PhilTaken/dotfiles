b4024e44ba5161ddf955478f4d3dd16b644f4bd6
Philipp Herzog <philipp.herzog@protonmail.com>
2021-03-14 12:01:51 +0100

    update nix flake command

--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
         elif [[ "$2" == "install" ]]; then
           sudo nixos-install --flake ".#$1" "${"\${@:3}"}"
         elif [[ "$2" == "upgrade" ]]; then
-          nix flake update --recreate-lock-file --commit-lock-file && sudo nixos-rebuild --flake ".#$1" switch
+          nix flake update --commit-lock-file && sudo nixos-rebuild --flake ".#$1" switch
         else
           sudo nixos-rebuild --flake ".#$1" "${"\${@:2}"}"
         fi
