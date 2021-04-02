ded694671fa6ed7f8e73ebeb5bf102037a073bb2
Philipp Herzog <philipp.herzog@protonmail.com>
2021-04-02 20:32:25 +0200

    fix nvim startup

--- users/modules/neovim/lua/custom/utils.lua
+++ users/modules/neovim/lua/custom/utils.lua
@@ -4,16 +4,18 @@ local uv = vim.loop
 local plugindir = "/home/nixos/Documents/personal/nvim_plugins/"
 
 local dir = uv.fs_opendir(plugindir, nil, 200)
-local entries = uv.fs_readdir(dir, nil)
-uv.fs_closedir(dir)
+if dir ~= nil then 
+	local entries = uv.fs_readdir(dir, nil)
+	uv.fs_closedir(dir)
 
-local dirs = vim.tbl_filter(function(entry)
-    return entry.type == "directory"
-end, entries)
+	local dirs = vim.tbl_filter(function(entry)
+	    return entry.type == "directory"
+	end, entries)
 
-vim.tbl_map(function(entry)
-    vim.api.nvim_exec("set runtimepath+=" .. plugindir .. entry.name, false)
-end, dirs)
+	vim.tbl_map(function(entry)
+	    vim.api.nvim_exec("set runtimepath+=" .. plugindir .. entry.name, false)
+	end, dirs)
+end
 
 --------------------------------------------------------------------------------
 -- for tab and shift-tab in completion
