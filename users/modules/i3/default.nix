eae79bb5da624629507d5f00be3c95e5cf14f7d8
Philipp Herzog <philipp.herzog@protonmail.com>
2021-04-05 14:56:23 +0200

    i3lock-fancy -> i3lock-pixeled, move extra packages from host definition

--- users/modules/i3/default.nix
+++ users/modules/i3/default.nix
@@ -33,7 +33,7 @@ in rec {
         "${modifier}+Return" = "exec ${terminal}";
         "${modifier}+d" = "kill";
         "${modifier}+space" = "exec ${pkgs.rofi}/bin/rofi -show run";
-        "${modifier}+l" = "exec ${pkgs.i3lock-fancy}/bin/i3lock-fancy -gp";
+        "${modifier}+l" = "exec ${pkgs.i3lock-pixeled}/bin/i3lock-pixeled";
         "${modifier}+1" = "workspace number 1";
         "${modifier}+2" = "workspace number 2";
         "${modifier}+3" = "workspace number 3";
@@ -101,7 +101,6 @@ in rec {
     };
     config = {
       "bar/top" = {
-        monitor = "\${env:MONITOR:eDP1}";
         width = "100%";
         height = "3%";
         radius = 0;
@@ -131,6 +130,9 @@ in rec {
     libnotify
     libappindicator
     glibcLocales
-    xorg.xf86inputsynaptics
+
+    i3status
+    i3lock-pixeled
+    i3blocks
   ];
 }
