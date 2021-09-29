7b7feaea8188819443f4d71b8055eb82c5dc79c3
Philipp Herzog <philipp.herzog@protonmail.com>
2021-09-29 12:58:12 +0200

    add defaults

--- lib/user.nix
+++ lib/user.nix
@@ -16,6 +16,42 @@ with builtins;
         systemd.user.startServices = true;
         home = {
           inherit username homeDirectory stateVersion;
+
+          # tbh no clue
+          sessionVariables = {
+            MOZ_USE_XINPUT2 = 1;
+            GTK_USE_PORTAL = 1;
+            AWT_TOOLKIT = "MToolkit";
+          };
+
+          packages = with pkgs; [
+            cacert
+            coreutils
+            qt5.qtbase
+            weather-icons
+            hicolor-icon-theme
+
+            iosevka-bin
+            (nerdfonts.override { fonts = [ "Iosevka" ]; })
+          ];
+        };
+
+        programs.home-manager.enable = true;
+        programs.zathura.enable = true;
+
+        # TODO move to own module? (gpg?)
+        # programs.gpg = {
+        #   enable = true;
+        #   #settings.default-key = usermod.gpgKey;
+        # };
+
+        services.syncthing.enable = true;
+
+        xdg = {
+          enable = true;
+          configHome = "${homeDirectory}/.config";
+          dataHome = "${homeDirectory}/.local/share";
+          cacheHome = "${homeDirectory}/.cache";
         };
 
         imports = [
