2a0983239c1219aef1126b6b7d71f73a6f96ea70
Philipp Herzog <philipp.herzog@protonmail.com>
2021-10-06 14:35:17 +0200

    feat: add laptop module

--- modules/hosts/laptop/default.nix
+++ modules/hosts/laptop/default.nix
@@ -1,4 +1,63 @@
 { pkgs
+, config
+, lib
 , ...
 }:
-{ }
+with lib;
+
+let
+  cfg = config.phil.template;
+in
+{
+  options.phil.template = {
+    enable = mkOption {
+      description = "enable template module";
+      type = types.bool;
+      default = false;
+    };
+
+    # "wlp0s20f3" for nixos-laptop
+    wirelessInterfaces = mkOption {
+      description = "list of wireless interfaces";
+      type = types.listOf types.str;
+      default = [ ];
+    };
+
+    enableVPN = mkOption {
+      description = "enable the mullvad vpn";
+      type = types.bool;
+      default = true;
+    };
+  };
+
+  config = mkIf (cfg.enable) {
+    environment = {
+      systemPackages = with pkgs; [
+        powertop
+        cmst # connman system tray
+      ];
+      sessionVariables.LIBVA_DRIVER_NAME = "iHD";
+    };
+
+    programs.light.enable = true;
+    services.xserver.libinput.touchpad.accelProfile = "flat";
+
+    services.connman = {
+      enable = true;
+      enableVPN = false;
+      wifi.backend = "wpa_supplicant";
+    };
+
+    networking = {
+      wg-quick.interfaces = mkIf (cfg.enableVPN) {
+        mullvad = import ../../../secret/vpn/mullvad.nix;
+      };
+      wireless = {
+        enable = true;
+        userControlled.enable = true;
+        interfaces = cfg.wirelessInterfaces;
+      };
+    };
+  };
+}
+
