441e3774f934061e163edd2b3cce506682ef615c
Philipp Herzog <philipp.herzog@protonmail.com>
2021-10-20 19:44:18 +0200

    wip: wireguard module

--- modules/hosts/wireguard/wireguard-peers.nix
+++ modules/hosts/wireguard/wireguard-peers.nix
@@ -1,23 +1,40 @@
+# alpha:
+#   - can ping: alpha, gamma
+#   - cant ping: beta
+# beta:
+#   - can ping: alpha, beta, gamma
+#   - cant ping:
+# gamma:
+#   - can ping: alpha, gamma
+#   - cant ping: beta
+
 rec {
   alpha = {
     publicKey = "LDOII0S7OWakg4oDrC1wUCoM/YXq3wXTEjYoqBbI2Sk=";
-    ownIPs = [ "fd2e:6bab:852b:1:1::1/72" ];
+    ownIPs = [ "10.100.0.1/24" ];
+    allowedIPs = [ "10.100.0.0/24" ];
     endpoint = "148.251.102.93";
-    port = 51820;
+    port = 51821;
   };
 
   beta = {
     publicKey = "/DWBidRPbNdqBhXZJFGpD20K+Bs6ViEbq4DJOlw5f0U=";
-    ownIPs = [ "fd2e:6bab:852b:2:1::1/72" ];
+    ownIPs = [ "10.100.0.2/24" ];
+    allowedIPs = [ "10.100.0.0/32" ];
+    endpoint = null;
   };
 
   gamma = {
     publicKey = "1w8CC/pEfXFPvdzyspDkuw/s8k2bkqAqk4KKg35IvQc=";
-    ownIPs = [ "fd2e:6bab:852b:2:2::1/72" ];
+    ownIPs = [ "10.100.0.3/24" ];
+    allowedIPs = [ "10.100.0.3/32" ];
+    endpoint = null;
   };
 
   nixos-laptop = {
     publicKey = "Xbi0ylobPYxxcxCvxaJ2mvC2WqGlODnMkeIYPG9tlVo=";
-    ownIPs = [ "fd2e:6bab:852b:2:3::1/72" ];
+    ownIPs = [ "10.100.0.4/24" ];
+    allowedIPs = [ "10.100.0.4/32" ];
+    endpoint = null;
   };
 }
