2a4150d26d1eec24280958664e039a61676bdc91
Philipp Herzog <philipp.herzog@protonmail.com>
2021-10-21 10:36:18 +0200

    formatting

--- modules/hosts/wireguard/default.nix
+++ modules/hosts/wireguard/default.nix
@@ -12,18 +12,20 @@ let
   peers = import ./wireguard-peers.nix;
   mkPeer = { publicKey, ownIPs, allowedIPs ? ownIPs, endpoint ? null, port ? 51821, persistentKeepalive ? 25, presharedKey ? null }: {
     inherit publicKey allowedIPs persistentKeepalive presharedKey;
-    endpoint = if endpoint != null then
-      "${endpoint}:${builtins.toString port}"
+    endpoint =
+      if endpoint != null then
+        "${endpoint}:${builtins.toString port}"
       else null;
   };
 
   foreignPeers = lib.filterAttrs (name: value: name != hostname) peers;
   foreignServerPeers = lib.filterAttrs (name: value: value.endpoint != null) foreignPeers;
 
-  peerlist = if (peers.${hostname}.endpoint != null) then
-    builtins.mapAttrs (name: mkPeer) foreignPeers
-  else
-    builtins.mapAttrs (name: mkPeer) foreignServerPeers;
+  peerlist =
+    if (peers.${hostname}.endpoint != null) then
+      builtins.mapAttrs (name: mkPeer) foreignPeers
+    else
+      builtins.mapAttrs (name: mkPeer) foreignServerPeers;
 
   listenPort = peers.${hostname}.port or 51821;
 in
@@ -55,14 +57,18 @@ in
       hosts = (builtins.listToAttrs
         (builtins.concatLists
           (builtins.map
-            (item: builtins.map (ip: {
-              name = builtins.elemAt (builtins.split "/" ip) 0;
-              value = [ "${item.name}.yggdrasil.vpn" ];
-            }) item.ips)
-            (lib.mapAttrsToList (name: value: {
-              name = name;
-              ips = value.ownIPs;
-            }) peers)
+            (item: builtins.map
+              (ip: {
+                name = builtins.elemAt (builtins.split "/" ip) 0;
+                value = [ "${item.name}.yggdrasil.vpn" ];
+              })
+              item.ips)
+            (lib.mapAttrsToList
+              (name: value: {
+                name = name;
+                ips = value.ownIPs;
+              })
+              peers)
           )
         )
       );
