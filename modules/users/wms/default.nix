754077e13d15dd57ac0f74ad0426acd3076536ef
Philipp Herzog <philipp.herzog@protonmail.com>
2022-05-22 02:56:34 +0200

    feat: add udiskie daemon

--- modules/users/wms/default.nix
+++ modules/users/wms/default.nix
@@ -2,13 +2,36 @@
 with lib;
 
 let
-  cfg = config.phil.wms.bars;
+  cfg = config.phil.wms;
+
+  # udiskie
+  commandArgs = concatStringsSep " " (map (opt: "-" + opt) [
+    "a"
+    "n"
+    "T"
+  ] ++ optional config.xsession.preferStatusNotifierItems "--appindicator");
 in {
-  options.phil.wms.bars = {
-    barcommand = mkOption {
+  options.phil.wms = {
+    udiskie.enable = mkEnableOption "udiskie";
+    bars.barcommand = mkOption {
       description = "comand to (re)start the bar(s)";
       type = types.str;
       default = "";
+    };
+  };
+
+  config = {
+    systemd.user.services.udiskie = mkIf (cfg.udiskie.enable) {
+      Unit = {
+        Description = "udiskie mount daemon";
+        #Requires = [ "tray.target" ];
+        After = [ "graphical-session-pre.target" ]; # "tray.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Service = { ExecStart = "${pkgs.udiskie}/bin/udiskie ${commandArgs}"; };
+
+      Install = { WantedBy = [ "graphical-session.target" ]; };
     };
   };
 
