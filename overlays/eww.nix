12b5d40dd62d4ed4a05089120df04167571b78ee
Philipp Herzog <philipp.herzog@protonmail.com>
2022-07-30 18:32:10 +0200

    feat: eww-base version name

--- overlays/eww.nix
+++ overlays/eww.nix
@@ -3,10 +3,14 @@ final: prev:
 let
   eww-base = withWayland: prev.rustPlatform.buildRustPackage rec {
     pname = "eww";
-    version = "git-${toString withWayland}";
+    version = "git-${if withWayland then "wayland" else "x"}";
 
     src = inputs.eww-src;
     cargoSha256 = "sha256-lTy3fIgBQAWODbBTDxqksA+aJHSF/xeFHMoHymVijk0=";
+
+    cargoLock = {
+      lockFile = "${inputs.eww-src}/Cargo.lock";
+    };
 
     nativeBuildInputs = [ prev.pkg-config ];
     buildInputs = [ prev.gtk3 ] ++ prev.lib.optional withWayland prev.gtk-layer-shell;
