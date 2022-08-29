4afb104290748441fb19561bab09cbe9f743c970
Philipp Herzog <philipp.herzog@protonmail.com>
2022-08-29 02:13:50 +0200

    feat: use the home-manager module to configure hyprland

--- modules/users/wms/hyprland/default.nix
+++ modules/users/wms/hyprland/default.nix
@@ -38,9 +38,10 @@ rec {
       MOZ_ENABLE_WAYLAND = 1;
     };
 
-    xdg.configFile."hypr/hyprland.conf".text =
+    wayland.windowManager.hyprland =
       let
-        terminal = "${pkgs.wezterm}/bin/wezterm";
+        terminal = "${pkgs.foot}/bin/foot";
+        #terminal = "${pkgs.wezterm}/bin/wezterm";
         #terminal = "${pkgs.alacritty}/bin/alacritty";
 
         #screenhsot = "${pkgs.flameshot}/bin/flameshot gui";
@@ -55,136 +56,138 @@ rec {
         up = "e";
         right = "o";
       in
-      ''
-        # This is an example Hyprland config file.
-        # Syntax is the same as in Hypr, but settings might differ.
-        #
-        # Refer to the wiki for more information.
+      {
+        enable = true;
+        extraConfig = ''
+          # This is an example Hyprland config file.
+          # Syntax is the same as in Hypr, but settings might differ.
+          #
+          # Refer to the wiki for more information.
 
-        monitor=,1920x1080@60,0x0,1
-        workspace=eDP-1,1
+          monitor=DP-1,2560x1440@75,0x1080,1
+          monitor=DVI-D-1,1920x1080@60,0x0,1
+          #workspace=DP-1,1
 
-        device:AT Translated Set 2 keyboard {
-          kb_layout=us(workman-intl),us(intl),
-          kb_options=caps:escape,grp:shifts_toggle,
-        }
+          device:AT Translated Set 2 keyboard {
+            kb_layout=us(workman-intl),us(intl),
+            kb_options=caps:escape,grp:shifts_toggle,
+          }
 
-        input {
-            kb_layout=us,
-            kb_options=caps:escape,
-        }
+          input {
+              kb_layout=us,
+              kb_options=caps:escape,
+          }
 
-        general {
-            sensitivity=1.0 # for mouse cursor
-            main_mod=SUPER
+          general {
+              sensitivity=1.0 # for mouse cursor
+              main_mod=SUPER
 
-            gaps_in=5
-            gaps_out=5
-            border_size=1
-            # col.active_border=0x66ee1111
-            col.active_border=0x66883333
-            col.inactive_border=0x66333333
+              gaps_in=5
+              gaps_out=5
+              border_size=1
+              # col.active_border=0x66ee1111
+              col.active_border=0x66883333
+              col.inactive_border=0x66333333
 
-            apply_sens_to_raw=0 # whether to apply the sensitivity to raw input (e.g. used by games where you aim using your mouse)
-            damage_tracking=full # leave it on full unless you hate your GPU and want to make it suffer
-        }
+              apply_sens_to_raw=0 # whether to apply the sensitivity to raw input (e.g. used by games where you aim using your mouse)
+              damage_tracking=full # leave it on full unless you hate your GPU and want to make it suffer
+          }
 
-        decoration {
-            rounding=5
-            blur=1
-            blur_size=3 # minimum 1
-            blur_passes=1 # minimum 1, more passes = more resource intensive.
-            # Your blur "amount" is blur_size * blur_passes, but high blur_size (over around 5-ish) will produce artifacts.
-            # if you want heavy blur, you need to up the blur_passes.
-            # the more passes, the more you can up the blur_size without noticing artifacts.
-        }
+          decoration {
+              rounding=5
+              blur=1
+              blur_size=3 # minimum 1
+              blur_passes=1 # minimum 1, more passes = more resource intensive.
+              # Your blur "amount" is blur_size * blur_passes, but high blur_size (over around 5-ish) will produce artifacts.
+              # if you want heavy blur, you need to up the blur_passes.
+              # the more passes, the more you can up the blur_size without noticing artifacts.
+          }
 
-        animations {
-            enabled=1
-            animation=windows,1,7,default
-            animation=border,1,10,default
-            animation=fade,1,10,default
-            animation=workspaces,1,6,default
-        }
+          animations {
+              enabled=1
+              animation=windows,1,7,default
+              animation=border,1,10,default
+              animation=fade,1,10,default
+              animation=workspaces,1,6,default
+          }
 
-        dwindle {
-            pseudotile=0 # enable pseudotiling on dwindle
-        }
+          dwindle {
+              pseudotile=0 # enable pseudotiling on dwindle
+          }
 
 
-        windowrule=float,Pinentry
-        windowrule=float,Media viewer
-        windowrule=float,title:Picture-in-Picture
+          windowrule=float,Pinentry
+          windowrule=float,Media viewer
+          windowrule=float,title:Picture-in-Picture
 
-        # example window rules
-        # for windows named/classed as abc and xyz
-        #windowrule=move 69 420,abc
-        #windowrule=size 420 69,abc
-        #windowrule=tile,xyz
-        #windowrule=float,abc
-        #windowrule=pseudo,abc
-        #windowrule=monitor 0,xyz
+          # example window rules
+          # for windows named/classed as abc and xyz
+          #windowrule=move 69 420,abc
+          #windowrule=size 420 69,abc
+          #windowrule=tile,xyz
+          #windowrule=float,abc
+          #windowrule=pseudo,abc
+          #windowrule=monitor 0,xyz
 
-        # example binds
-        bind=SUPER,P,pseudo,
-        bind=SUPER,D,killactive,
-        bind=SUPER,C,exit,
-        bind=SUPERSHIFT,space,togglefloating,
+          # example binds
+          bind=SUPER,P,pseudo,
+          bind=SUPER,D,killactive,
+          bind=SUPER,C,exit,
+          bind=SUPERSHIFT,space,togglefloating,
 
-        bind=SUPER,space,exec,${menu}
-        bind=SUPER,return,exec,${terminal}
-        bind=SUPER,l,exec,${lock}
-        bind=SUPER,q,exec,${screenshot}
+          bind=SUPER,space,exec,${menu}
+          bind=SUPER,return,exec,${terminal}
+          bind=SUPER,l,exec,${lock}
+          bind=SUPER,q,exec,${screenshot}
 
-        bind=SUPER,${left},movefocus,l
-        bind=SUPER,${down},movefocus,d
-        bind=SUPER,${up},movefocus,u
-        bind=SUPER,${right},movefocus,r
+          bind=SUPER,${left},movefocus,l
+          bind=SUPER,${down},movefocus,d
+          bind=SUPER,${up},movefocus,u
+          bind=SUPER,${right},movefocus,r
 
-        bind=SUPERCONTROL,${left},movecurrentworkspacetomonitor,l
-        bind=SUPERCONTROL,${down},movecurrentworkspacetomonitor,d
-        bind=SUPERCONTROL,${up},movecurrentworkspacetomonitor,u
-        bind=SUPERCONTROL,${right},movecurrentworkspacetomonitor,r
+          bind=SUPERCONTROL,${left},movecurrentworkspacetomonitor,l
+          bind=SUPERCONTROL,${down},movecurrentworkspacetomonitor,d
+          bind=SUPERCONTROL,${up},movecurrentworkspacetomonitor,u
+          bind=SUPERCONTROL,${right},movecurrentworkspacetomonitor,r
 
-        bind=SUPER,1,workspace,1
-        bind=SUPER,2,workspace,2
-        bind=SUPER,3,workspace,3
-        bind=SUPER,4,workspace,4
-        bind=SUPER,5,workspace,5
-        bind=SUPER,6,workspace,6
-        bind=SUPER,7,workspace,7
-        bind=SUPER,8,workspace,8
-        bind=SUPER,9,workspace,9
-        bind=SUPER,0,workspace,10
+          bind=SUPER,1,workspace,1
+          bind=SUPER,2,workspace,2
+          bind=SUPER,3,workspace,3
+          bind=SUPER,4,workspace,4
+          bind=SUPER,5,workspace,5
+          bind=SUPER,6,workspace,6
+          bind=SUPER,7,workspace,7
+          bind=SUPER,8,workspace,8
+          bind=SUPER,9,workspace,9
+          bind=SUPER,0,workspace,10
 
-        bind=SUPERSHIFT,exclam,movetoworkspace,1
-        bind=SUPERSHIFT,at,movetoworkspace,2
-        bind=SUPERSHIFT,numbersign,movetoworkspace,3
-        bind=SUPERSHIFT,dollar,movetoworkspace,4
-        bind=SUPERSHIFT,percent,movetoworkspace,5
-        bind=SUPERSHIFT,asciicircum,movetoworkspace,6
-        bind=SUPERSHIFT,ampersand,movetoworkspace,7
-        bind=SUPERSHIFT,asterisk,movetoworkspace,8
-        bind=SUPERSHIFT,parenleft,movetoworkspace,9
-        bind=SUPERSHIFT,parenright,movetoworkspace,10
+          bind=SUPERSHIFT,1,movetoworkspace,1
+          bind=SUPERSHIFT,2,movetoworkspace,2
+          bind=SUPERSHIFT,3,movetoworkspace,3
+          bind=SUPERSHIFT,4,movetoworkspace,4
+          bind=SUPERSHIFT,5,movetoworkspace,5
+          #bind=SUPERSHIFT,6,movetoworkspace,6
+          bind=SUPERSHIFT,7,movetoworkspace,7
+          bind=SUPERSHIFT,8,movetoworkspace,8
+          bind=SUPERSHIFT,9,movetoworkspace,9
+          bind=SUPERSHIFT,0,movetoworkspace,10
 
-        bind=,XF86MonBrightnessUp,exec,light -T 1.4
-        bind=,XF86MonBrightnessDown,exec,light -T 0.72
-        bind=,XF86AudioMute,exec,${pkgs.pamixer}/bin/pamixer -t
-        bind=,XF86AudioMicMute,exec,pactl set-source-mute @DEFAULT_SOURCE@ toggle
-        bind=,XF86AudioLowerVolume,exec,${pkgs.pamixer}/bin/pamixer -ud 2
-        bind=,XF86AudioRaiseVolume,exec,${pkgs.pamixer}/bin/pamixer -ui 2
-        bind=,XF86AudioPrev,exec,${pkgs.playerctl}/bin/playerctl previous
-        bind=,XF86AudioNext,exec,${pkgs.playerctl}/bin/playerctl next
-        bind=,XF86AudioPlay,exec,${pkgs.playerctl}/bin/playerctl play-pause
+          bind=,XF86MonBrightnessUp,exec,light -T 1.4
+          bind=,XF86MonBrightnessDown,exec,light -T 0.72
+          bind=,XF86AudioMute,exec,${pkgs.pamixer}/bin/pamixer -t
+          bind=,XF86AudioMicMute,exec,pactl set-source-mute @DEFAULT_SOURCE@ toggle
+          bind=,XF86AudioLowerVolume,exec,${pkgs.pamixer}/bin/pamixer -ud 2
+          bind=,XF86AudioRaiseVolume,exec,${pkgs.pamixer}/bin/pamixer -ui 2
+          bind=,XF86AudioPrev,exec,${pkgs.playerctl}/bin/playerctl previous
+          bind=,XF86AudioNext,exec,${pkgs.playerctl}/bin/playerctl next
+          bind=,XF86AudioPlay,exec,${pkgs.playerctl}/bin/playerctl play-pause
 
-        exec-once=systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-        exec-once=${pkgs.swaybg}/bin/swaybg -i ${cfg.background_image}
-        exec-once=${barcommand}
+          exec-once=systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+          exec-once=${pkgs.swaybg}/bin/swaybg -i ${cfg.background_image}
+          exec-once=${barcommand}
+        '';
+    };
 
-      '';
-
-    # exec-once=xrandr --output \"DP-0\" --below \"DVI-D-0\" --primary --mode 2560x1440 --rate 74.97
 
     # TODO: move someplace else
     programs = {
