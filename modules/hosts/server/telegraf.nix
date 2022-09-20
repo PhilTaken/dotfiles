5df9608b9e3a5ec18c8361542e68275488593412
Philipp Herzog <philipp.herzog@protonmail.com>
2022-09-20 23:28:59 +0200

    fix: telegraf config on alpha

--- modules/hosts/server/telegraf.nix
+++ modules/hosts/server/telegraf.nix
@@ -7,7 +7,8 @@ with lib;
 
 let
   cfg = config.phil.server.services.telegraf;
-  outputUrl = "http://influx.pherzog.xyz:8086";
+  #outputUrl = "http://influx.pherzog.xyz:8086";
+  outputUrl = "http://10.200.0.5:8086";
 in
 {
   options.phil.server.services.telegraf = {
