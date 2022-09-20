9524846316bc2afae63c0347552e03e90e76bc9c
Philipp Herzog <philipp.herzog@protonmail.com>
2022-09-20 10:12:39 +0200

    feat: nvidia env vars

--- modules/hosts/video/default.nix
+++ modules/hosts/video/default.nix
@@ -134,17 +134,17 @@ in
     # https://wiki.hyprland.org/Nvidia/#how-to-get-hyprland-to-possibly-work-on-nvidia
     environment.variables = mkIf (cfg.driver == "nvidia") {
       NIXOS_OZONE_WL = "1";
-      #LIBVA_DRIVER_NAME = "nvidia";
+      LIBVA_DRIVER_NAME = "nvidia";
       #CLUTTER_BACKEND = "wayland";
       #XDG_SESSION_TYPE = "wayland";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
       MOZ_ENABLE_WAYLAND = "1";
-      #GBM_BACKEND = "nvidia-drm";
-      #__GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      GBM_BACKEND = "nvidia-drm";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
       WLR_NO_HARDWARE_CURSORS = "1";
-      #WLR_BACKEND = "vulkan";
-      #QT_QPA_PLATFORM = "wayland";
-      #GDK_BACKEND = "wayland";
+      WLR_BACKEND = "vulkan";
+      QT_QPA_PLATFORM = "wayland";
+      GDK_BACKEND = "wayland";
     };
 
     xdg = {
