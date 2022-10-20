7a68eb85e72aec09eb0044c09a5e57f151f84abb
Philipp Herzog <philipp.herzog@protonmail.com>
2022-10-20 16:23:33 +0200

    feat: remove snow-agent

--- custom_pkgs/default.nix
+++ custom_pkgs/default.nix
@@ -2,7 +2,6 @@ final: prev: {
   zettlr = prev.callPackage ./applications/zettlr { };
   #calibre = prev.callPackage ./applications/calibre { };
   fortls = prev.callPackage ./applications/fortls { };
-  snow-agent = prev.callPackage ./applications/snow-agent { };
   goneovim = prev.callPackage ./applications/goneovim { };
   zk = prev.callPackage ./applications/zk { };
 
