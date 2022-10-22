cb58c71861953292bffb3113c19782abe5e558aa
Philipp Herzog <philipp.herzog@protonmail.com>
2022-10-23 01:50:20 +0200

    feat: nix-serve module

--- modules/hosts/server/nix-serve.nix
+++ modules/hosts/server/nix-serve.nix
@@ -10,15 +10,32 @@ let
 in
 {
   options.phil.server.services.nix-serve = {
-    enable = mkEnableOption "serve nix store via ssh";
+    enable = mkEnableOption "sharing nix store";
+    ng = mkOption {
+      type = types.bool;
+      default = false;
+    };
+
+    host = mkOption {
+      type = types.str;
+      default = "nix-store";
+    };
+
+    port = mkOption {
+      type = types.port;
+      default = 5000;
+    };
   };
 
   config = mkIf cfg.enable {
-    # maybe: https serving?
+    sops.secrets.nix-serve-secret-key = {};
+    phil.server.services.caddy.proxy.${cfg.host} = cfg.port;
 
-    nix.sshServe.enable = true;
-    nix.sshServe.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCoABVjBx1az00D8EBjw9/NS9luqO2lN4Y87/2xsQqPTx9P7aXzfX53TwmU9Wpmp7qOIKykd8GSkBdCizMEzgaGmJl6+Di2GYvEfN0NrsLdBrjmIh7AQyR6UbY7qoTFjZ28864rk9QV9to2R1APL7o1wzdmCrFtTsemV+lw9MglqcPLT+ae2mba9fD84FFDmcSJMg5x1QHrO5GuWg/Ng7SE1eRhDbDmz66+HhdgvRRDJ9VwPGcH5ruXsDKvi/nrLVSxw7afvuM5KcNYoy+9CrA/N10cO5zdn4/q2DLYujkOvAucCDJ4bUEe8q6xEZw1LfCjKWIoFxzt+hetfkjS/Y3wWWTcHfcOx/BV6cOxyAFUGbu9RX/iUpyt8LAfjQv6L1zcD7vxYpfKz88jI/4zL7mHwILg+XQklBeiBsEQ4PyO1+4oIfuju241hVk+bFZYUD+AzzCNv7GKNNHe4aa4MWN6RLLhNxe9QlOTnsw0l2XNypr62Q1V8nxZkSY7mW8Hn0hLxTT82mTLuAff2yHPu+w+i0ELkk0BO28apxU1dPPbScHvojRlXTwIBvH3HN6TWdj2YnNFMdGvZgxxFNbi4l/7Gar1FKgi79KOwcm89ATmjONfbQMub+TaeBACefMZ9Q7uzbWeNO3mZpVA8nvM5eleqLemxYoeAQBuYjBjJlAHzQ== cardno:000614321676"
-    ];
+    services.nix-serve = {
+      enable = true;
+      package = if cfg.ng then pkgs.haskellPackages.nix-serve-ng else pkgs.nix-serve;
+      secretKeyFile = config.sops.secrets.nix-serve-secret-key.path;
+      inherit (cfg) port;
+    };
   };
 }
