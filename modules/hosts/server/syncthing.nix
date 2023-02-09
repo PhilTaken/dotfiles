c90d5a06ddeb91bc1e05df5cbe8b5423388ebe7a
Philipp Herzog <philipp.herzog@protonmail.com>
2023-02-09 18:00:05 +0100

    wip: syncthing config for all hosts

--- modules/hosts/server/syncthing.nix
+++ modules/hosts/server/syncthing.nix
@@ -7,31 +7,58 @@
 let
   inherit (lib) mkOption mkIf types mkEnableOption;
   cfg = config.phil.server.services.syncthing;
+
+  sopsConfig = {
+    sopsFile = ../../../sops/machines + "/${config.networking.hostName}.yaml";
+    owner = config.systemd.services."syncthing".serviceConfig.User or "syncthing";
+  };
 in
 {
 
   options.phil.server.services.syncthing = {
     enable = mkEnableOption "syncthing service";
-    dataFolder = mkOption {
+    dataDir = mkOption {
       description = "folder for synced folders";
       type = types.str;
       default = "/media/syncthing/data";
     };
-    configFolder = mkOption {
+    configDir = mkOption {
       description = "folder for syncthing config";
       type = types.str;
       default = "/media/syncthing/config";
     };
+
+    override = mkEnableOption "override folders and devices";
+
+    openDefaultPorts = mkEnableOption "open default ports";
   };
 
   config = mkIf cfg.enable {
+    sops.secrets.syncthing-cert = sopsConfig;
+    sops.secrets.syncthing-key = sopsConfig;
+
     services.syncthing = {
-      enable = true;
-      user = "nixos";
-      dataDir = cfg.dataFolder;
-      configDir = cfg.configFolder;
-      openDefaultPorts = true;
+      inherit (cfg) openDefaultPorts enable configDir dataDir;
+
       guiAddress = "0.0.0.0:8384";
+
+      key = config.sops.secrets.syncthing-key.path;
+      cert = config.sops.secrets.syncthing-cert.path;
+
+      devices = {
+      };
+
+      folders = {
+        "" = {
+        };
+      };
+
+      overrideFolders = cfg.override;
+      overrideDevices = cfg.override;
+
+      extraOptions = {
+        gui.theme = "black";
+      };
     };
   };
 }
