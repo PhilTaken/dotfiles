5f08da75a7d1aa880edab35e30c3ab77516905e5
Philipp Herzog <philipp.herzog@protonmail.com>
2023-04-02 21:31:07 +0200

    feat: vector->promtail for syslog

--- modules/hosts/server/vector.nix
+++ modules/hosts/server/vector.nix
@@ -17,8 +17,37 @@ in
   };
 
   config = mkIf cfg.enable {
-    services.vector = {
+    services.promtail = {
       enable = true;
+      configuration = {
+        server = {
+          http_listen_port = 28183;
+          grpc_listen_port = 0;
+        };
+
+        positions.filename = "/tmp/positions.yaml";
+
+        clients = [{ url = "https://loki.${net.tld}/loki/api/v1/push"; }];
+
+        scrape_configs = [{
+          job_name = "journal";
+          journal = {
+            max_age = "12h";
+            labels = {
+              job = "systemd-journal";
+              host = config.networking.hostName;
+            };
+          };
+          relabel_configs = [{
+            source_labels = ["__journal__systemd_unit"];
+            target_label = "unit";
+          }];
+        }];
+      };
+    };
+
+    services.vector = {
+      enable = false;
       journaldAccess = true;
 
       settings = {
