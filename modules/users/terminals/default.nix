1a4d1b8e84fd16674cffa0b3b8cc42068a1c2007
Philipp Herzog <ph@flyingcircus.io>
2023-08-17 09:33:41 +0200

    feat: adjust wezterm cnofig

--- modules/users/terminals/default.nix
+++ modules/users/terminals/default.nix
@@ -55,12 +55,13 @@ in {
         enable = true;
         extraConfig = ''
           return {
-            window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+            window_padding = { left = 5, right = 5, top = 5, bottom = 5 },
             hide_tab_bar_if_only_one_tab = true,
             color_scheme = "Catppuccin Mocha",
             font = wezterm.font("${cfg.default_font}"),
             font_size = 11.0,
             dpi = 192.0,
+            adjust_window_size_when_changing_font_size = false,
             -- front_end = "WebGpu",
           }
         '';
