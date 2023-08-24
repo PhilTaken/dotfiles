4c9ff21cacc79442c82b2efbf6127303f5b3ffbc
Philipp Herzog <ph@flyingcircus.io>
2023-08-24 10:10:18 +0200

    very wip: racket langserver, disable for now

--- custom_pkgs/racket-langserver/default.nix
+++ custom_pkgs/racket-langserver/default.nix
@@ -1,23 +1,55 @@
-# WIP
-{
-  stdenv,
-  fetchFromGitHub,
-  ...
-}:
-stdenv.mkDerivation {
-  pname = "racket-langserver";
-  version = "0.0.1";
+{hello, ...}: hello
+#{ fetchFromGitHub,
+#racket2nix ? fetchFromGitHub {
+#owner = "fractalide";
+#repo = "racket2nix";
+#rev = "59c614406d4796f40620f6490b0b05ecb51ed976";
+#sha256 = "0z5y1jm60vkwvi66q39p88ygkgyal81486h577gikmpqjxkg9d6i";
+#},
+#pkgs,
+#...
+#}: let
+# maybe do this here?
+#pkgs = {
+#pkgs ? import (import ../nixpkgs) {},
+#system ? builtins.currentSystem,
+#racket-minimal ? pkgs.racket-minimal,
+#...}@args: let
+#nixpkgs = pkgs;
+#inherit (nixpkgs) bash lib newScope racket;
+#inherit (lib) makeScope;
+#in
+#makeScope newScope (self: {
+#pkgs = self;
+#callPackageFull = (makeScope self.newScope (fullself: nixpkgs // self //
+#{ pkgs = fullself; extend = fullself.overrideScope'; })).callPackage;
+#extend = self.overrideScope';
+#racket-full = racket;
+#inherit racket-minimal;
+#racket = self.racket-minimal;
+#buildDrvs = name: buildInputs: derivation {
+#inherit name buildInputs system;
+#builder = bash + "/bin/bash";
+#args = [ "-c" "echo -n > $out" ];
+#};
+#racket2nix-stage0 = self.callPackage ../stage0.nix {};
+#racket2nix-stage1 = self.callPackage ../stage1.nix {};
+#racket2nix = self.buildRacketPackage "racket2nix";
+#inherit (self.callPackage ../build-racket.nix {})
+#buildRacket buildRacketPackage buildRacketCatalog
+#buildThinRacket buildThinRacketPackage;
+#});
+#r2nix = import racket2nix {
+#pkgs = import "${racket2nix}/pkgs" {
+#inherit (pkgs) system;
+#};
+#inherit (pkgs) system;
+#};
+#langserver-src = fetchFromGitHub {
+#owner = "jeapostrophe";
+#repo = "racket-langserver";
+#rev = "3c3c217cced27788fb342ffcc82c87714b5a8082";
+#sha256 = "0000000000000000000000000000000000000000000000000000";
+#};
+#in r2nix.buildRacketPackage langserver-src
 
-  src = fetchFromGitHub {
-    owner = "jeapostrophe";
-    repo = "racket-langserver";
-    rev = "3447fe6b5091c78f9807bcaf6ee7640c862a9484";
-    sha256 = "049wkw40jyd6zivn7c7wahwicfb5lbaj5vrfn1gsvk1z9yh8yp1s";
-  };
-
-  buildPhase = ''
-    mkdir -p $out/bin
-    echo "echo test" > $out/bin/racket-langserver
-    chmod +x $out/bin/racket-langserver
-  '';
-}
