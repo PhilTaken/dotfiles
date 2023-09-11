57fc7704a5a2344a7b9e52cd669f9122df3b9a77
Philipp Herzog <ph@flyingcircus.io>
2023-09-11 10:16:22 +0200

    fix: devshell

--- modules/flake/shells.nix
+++ modules/flake/shells.nix
@@ -172,14 +172,14 @@ in {
           help = "sign all configurations";
           command = lib.concatStrings (lib.mapAttrsToList
             (name: ip: let
-              cidr = builtins.elemAt (lib.splitString "/" net.networks.milkyway.gateway) 1;
+              cidr = builtins.elemAt (lib.splitString "/" net.networks.milkyway.netmask) 1;
             in ''
               ${pkgs.nebula}/bin/nebula-cert sign \
                 -ca-crt /run/secrets/ca \
                 -ca-key /run/secrets/key \
                 -name ${name} -ip ${ip}/${cidr}
             '')
-            (lib.filterAttrs (n: _: ! builtins.elem n ["interfaceName" "gateway"]) net.networks.milkyway));
+            net.networks.milkyway.hosts);
         }
 
         {
