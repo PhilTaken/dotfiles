8c320a326c7ef4bf550f54dbc13955d6a7405725
Philipp Herzog <ph@flyingcircus.io>
2024-02-20 18:19:20 +0100

    feat: use upstream dns server when not connected to nebula

--- modules/hosts/dns/default.nix
+++ modules/hosts/dns/default.nix
@@ -23,32 +23,51 @@ in {
       type = types.nullOr (types.enum hostnames);
       description = "dns host";
       example = "gamma";
-      #default = null;
       default = default_nameserver;
     };
   };
 
-  config = mkIf (cfg.nameserver != null && config.phil.nebula.enable) {
-    #networking.networkmanager.dns = mkIf (config.networking.networkmanager.enable == true) "none";
+  config = lib.mkMerge [
+    # use public nameserver when not connected to nebula
+    (mkIf (!config.phil.nebula.enable) {
+      networking.nameservers = ["9.9.9.9"];
 
-    networking.nameservers =
-      if same-server
-      then ["localhost"]
-      else [
-        "${iplot.${cfg.nameserver}}#dns.${net.tld}"
-        "2a0e:dc0:6:23::2#dot-ch.blahdns.com"
-      ];
+      services.resolved = {
+        enable = true;
 
-    services.resolved = {
-      enable = ! same-server;
+        fallbackDns = ["1.1.1.1"];
 
-      fallbackDns = ["2a0e:dc0:6:23::2#dot-ch.blahdns.com"];
+        extraConfig = ''
+          DNSOverTLS=yes
+        '';
 
-      extraConfig = ''
-        DNSOverTLS=yes
-      '';
+        dnssec = "false";
+      };
+    })
 
-      dnssec = "false";
-    };
-  };
+    # otherwise just use the one on the nebula network
+    (mkIf (cfg.nameserver != null && config.phil.nebula.enable) {
+      #networking.networkmanager.dns = mkIf (config.networking.networkmanager.enable == true) "none";
+
+      networking.nameservers =
+        if same-server
+        then ["localhost"]
+        else [
+          "${iplot.${cfg.nameserver}}#dns.${net.tld}"
+          "2a0e:dc0:6:23::2#dot-ch.blahdns.com"
+        ];
+
+      services.resolved = {
+        enable = ! same-server;
+
+        fallbackDns = ["2a0e:dc0:6:23::2#dot-ch.blahdns.com"];
+
+        extraConfig = ''
+          DNSOverTLS=yes
+        '';
+
+        dnssec = "false";
+      };
+    })
+  ];
 }
