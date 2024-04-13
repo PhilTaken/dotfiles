64305f84d65a8cf13f53bd2449922de5d5546f2d
Philipp Herzog <ph@flyingcircus.io>
2024-04-13 10:19:43 +0200

    feat: add beta to deploy config

--- modules/flake/deploy.nix
+++ modules/flake/deploy.nix
@@ -15,6 +15,13 @@ in {
         profiles.system.path = (activateFor "x86_64-linux") self.nixosConfigurations.alpha;
       };
 
+      beta = {
+        hostname = "10.200.0.2";
+        sshUser = "root";
+        remoteBuild = true;
+        profiles.system.path = (activateFor "aarch64-linux") self.nixosConfigurations.beta;
+      };
+
       delta = {
         hostname = "10.200.0.5";
         sshUser = "root";
