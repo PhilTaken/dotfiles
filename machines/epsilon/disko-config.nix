482d0b61580da9e2c329ef2365cafda295230a04
Philipp Herzog <ph@flyingcircus.io>
2024-04-13 10:30:46 +0200

    fix: update epsilon disko config

--- machines/epsilon/disko-config.nix
+++ machines/epsilon/disko-config.nix
@@ -4,33 +4,27 @@
       device = builtins.elemAt disks 0;
       type = "disk";
       content = {
-        type = "table";
-        format = "gpt";
-        partitions = [
-          {
-            name = "ESP";
+        type = "gpt";
+        partitions = {
+          ESP = {
             start = "1MiB";
             end = "100MiB";
-            bootable = true;
             content = {
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
             };
-          }
-          {
-            name = "root";
+          };
+          root = {
             start = "100MiB";
             end = "100%";
-            part-type = "primary";
-            bootable = true;
             content = {
               type = "filesystem";
               format = "ext4";
               mountpoint = "/";
             };
-          }
-        ];
+          };
+        };
       };
     };
   };
