9e13b8951bf8ff31660d9fde19fe11a1ae61b2f4
Philipp Herzog <ph@flyingcircus.io>
2024-05-12 07:27:55 +0200

    nextcloud formatting

--- modules/hosts/server/nextcloud.nix
+++ modules/hosts/server/nextcloud.nix
@@ -30,14 +30,17 @@ in {
     localAddress = "192.0.0.2";
   in
     mkIf cfg.enable {
-      sops.secrets.nextcloud-adminpass = {
-        mode = "777";
-      };
+      sops.secrets.nextcloud-adminpass.mode = "777";
 
       networking.nat = {
         enable = true;
         internalInterfaces = ["ve-+"];
         externalInterface = "enp1s0";
+      };
+
+      networking.firewall.interfaces."${net.networks.default.interfaceName}" = {
+        allowedUDPPorts = [cfg.port];
+        allowedTCPPorts = [cfg.port];
       };
 
       phil.backup.jobs."nextcloud" = {
@@ -74,199 +77,185 @@ in {
         home = "/media/nextcloud";
         datadir = "/var/lib/nextcloud";
         hostName = "${cfg.host}.${net.tld}";
-      in
-        mkIf cfg.enable {
-          ephemeral = false;
-          autoStart = true;
+      in {
+        ephemeral = false;
+        autoStart = true;
 
-          #privateNetwork = false;
-          privateNetwork = true;
-          inherit localAddress hostAddress;
+        #privateNetwork = false;
+        privateNetwork = true;
+        inherit localAddress hostAddress;
 
-          forwardPorts = [
-            {
-              containerPort = 80;
-              hostPort = cfg.port;
-              protocol = "tcp";
-            }
-          ];
+        forwardPorts = [
+          {
+            containerPort = 80;
+            hostPort = cfg.port;
+            protocol = "tcp";
+          }
+        ];
 
-          bindMounts = {
-            ${adminpassFile} = {
-              hostPath = adminpassFile;
-              isReadOnly = true;
-            };
-
-            ${datadir} = {
-              hostPath = cfg.datadir;
-              isReadOnly = false;
-            };
-
-            ${home} = {
-              hostPath = home;
-              isReadOnly = false;
-            };
+        bindMounts = {
+          ${adminpassFile} = {
+            hostPath = adminpassFile;
+            isReadOnly = true;
           };
 
-          config = {
-            config,
-            pkgs,
-            ...
-          }: {
-            # https://github.com/NixOS/nixpkgs/issues/162686
-            networking.nameservers = ["1.1.1.1"];
-            # WORKAROUND
-            environment.etc."resolv.conf".text = "nameserver 1.1.1.1";
+          ${datadir} = {
+            hostPath = cfg.datadir;
+            isReadOnly = false;
+          };
 
-            networking.firewall.enable = false;
-
-            environment.systemPackages = [
-              (pkgs.writeShellScriptBin "fix-pics" ''
-                #!/bin/sh
-
-                albumdir=$1
-
-                echo "Changing modified date to shot date..."
-                ${pkgs.exiftool}/bin/exiftool "-filemodifydate<datetimeoriginal" -r "$albumdir"
-
-                echo "Renaming files to shot date..."
-                ${pkgs.exiftool}/bin/exiftool '-FileName<DateTimeOriginal' -r -d "%Y-%m-%d_%H.%M.%S%%-c.%%e" "$albumdir"
-
-                # uncomment the below command if running on the server
-                echo "Re-scanning your Nextcloud data directory..."
-                ${config.services.nextcloud.occ}/bin/nextcloud-occ files:scan --all
-
-                exit 0
-              '')
-            ];
-
-            #services.nginx.virtualHosts.${hostName}.listen = [
-            #{
-            #addr = localAddress;
-            #port = 80;
-            #}
-            #];
-
-            services.nextcloud = let
-              news = pkgs.fetchNextcloudApp {
-                appName = "news";
-                appVersion = "25.0.0-alpha5";
-                url = "https://github.com/nextcloud/news/releases/download/25.0.0-alpha5/news.tar.gz";
-                sha256 = "sha256-BbGzrOBDshZfiDhKUMiTXGnI7767hpCGsujMbPqmJyg=";
-                license = "agpl3Plus";
-              };
-            in {
-              enable = true;
-              package = pkgs.nextcloud28;
-
-              inherit home datadir hostName;
-              https = true;
-
-              extraApps = {
-                inherit
-                  (pkgs.nextcloud28Packages.apps)
-                  calendar
-                  bookmarks
-                  contacts
-                  deck
-                  groupfolders
-                  impersonate
-                  spreed
-                  twofactor_webauthn
-                  previewgenerator
-                  ;
-                inherit news;
-                # "onlyoffice" "tasks"
-              };
-
-              caching.redis = true;
-              caching.apcu = false;
-
-              configureRedis = true;
-              notify_push = {
-                enable = true;
-              };
-
-              config = {
-                adminuser = "nc-admin";
-                inherit adminpassFile;
-                dbtype = "pgsql";
-                dbhost = "/run/postgresql";
-                dbname = "nextcloud";
-                dbuser = "nextcloud";
-              };
-
-              settings = {
-                default_phone_region = "DE";
-                overwriteprotocol = "https";
-                trusted_proxies = [
-                  "10.200.0.1"
-                  "10.200.0.5"
-                ];
-                redis = {
-                  host = "/run/redis-nextcloud/redis.sock";
-                  port = 0;
-                };
-                "memcache.local" = "\\OC\\Memcache\\Redis";
-                "memcache.distributed" = "\\OC\\Memcache\\Redis";
-                "memcache.locking" = "\\OC\\Memcache\\Redis";
-
-                preview_max_x = 2048;
-                preview_max_y = 2048;
-                jpeg_quality = 60;
-              };
-            };
-
-            services.redis.servers.nextcloud = {
-              enable = true;
-              user = "nextcloud";
-              port = 0;
-            };
-
-            services.postgresql = {
-              enable = true;
-              package = pkgs.postgresql_14;
-              ensureUsers = [
-                {
-                  name = "nextcloud";
-                  ensureDBOwnership = true;
-                }
-              ];
-              ensureDatabases = ["nextcloud"];
-            };
-
-            systemd.services."nextcloud-setup" = {
-              requires = ["postgresql.service"];
-              after = ["postgresql.service"];
-            };
-
-            systemd.timers."nextcloud-preview-gen" = {
-              wantedBy = ["timers.target"];
-              after = ["nextcloud-setup.service"];
-              timerConfig = {
-                OnBootSec = "10m";
-                OnUnitActiveSec = "10m";
-                Unit = "nextcloud-preview-gen.service";
-              };
-            };
-
-            systemd.services."nextcloud-preview-gen" = {
-              script = ''
-                ${config.services.nextcloud.occ}/bin/nextcloud-occ preview:pre-generate
-              '';
-
-              serviceConfig = {
-                Type = "oneshot";
-                User = "root";
-              };
-            };
-
-            system.stateVersion = "22.11";
+          ${home} = {
+            hostPath = home;
+            isReadOnly = false;
           };
         };
-      networking.firewall.interfaces."${net.networks.default.interfaceName}" = {
-        allowedUDPPorts = [cfg.port];
-        allowedTCPPorts = [cfg.port];
+
+        config = {
+          config,
+          pkgs,
+          ...
+        }: {
+          # https://github.com/NixOS/nixpkgs/issues/162686
+          networking.nameservers = ["1.1.1.1"];
+          # WORKAROUND
+          environment.etc."resolv.conf".text = "nameserver 1.1.1.1";
+
+          networking.firewall.enable = false;
+
+          environment.systemPackages = [
+            (pkgs.writeShellScriptBin "fix-pics" ''
+              #!/bin/sh
+
+              albumdir=$1
+
+              echo "Changing modified date to shot date..."
+              ${pkgs.exiftool}/bin/exiftool "-filemodifydate<datetimeoriginal" -r "$albumdir"
+
+              echo "Renaming files to shot date..."
+              ${pkgs.exiftool}/bin/exiftool '-FileName<DateTimeOriginal' -r -d "%Y-%m-%d_%H.%M.%S%%-c.%%e" "$albumdir"
+
+              # uncomment the below command if running on the server
+              echo "Re-scanning your Nextcloud data directory..."
+              ${config.services.nextcloud.occ}/bin/nextcloud-occ files:scan --all
+
+              exit 0
+            '')
+          ];
+
+          services.nextcloud = let
+            news = pkgs.fetchNextcloudApp {
+              appName = "news";
+              appVersion = "25.0.0-alpha5";
+              url = "https://github.com/nextcloud/news/releases/download/25.0.0-alpha5/news.tar.gz";
+              sha256 = "sha256-BbGzrOBDshZfiDhKUMiTXGnI7767hpCGsujMbPqmJyg=";
+              license = "agpl3Plus";
+            };
+          in {
+            enable = true;
+            package = pkgs.nextcloud28;
+
+            inherit home datadir hostName;
+            https = true;
+
+            extraApps = {
+              inherit
+                (pkgs.nextcloud28Packages.apps)
+                calendar
+                bookmarks
+                contacts
+                deck
+                groupfolders
+                impersonate
+                spreed
+                twofactor_webauthn
+                previewgenerator
+                ;
+              inherit news;
+              # "onlyoffice" "tasks"
+            };
+
+            caching.redis = true;
+            caching.apcu = false;
+
+            configureRedis = true;
+            notify_push.enable = true;
+
+            config = {
+              adminuser = "nc-admin";
+              inherit adminpassFile;
+              dbtype = "pgsql";
+              dbhost = "/run/postgresql";
+              dbname = "nextcloud";
+              dbuser = "nextcloud";
+            };
+
+            settings = {
+              default_phone_region = "DE";
+              overwriteprotocol = "https";
+              trusted_proxies = [
+                "10.200.0.1"
+                "10.200.0.5"
+              ];
+              redis = {
+                host = "/run/redis-nextcloud/redis.sock";
+                port = 0;
+              };
+              "memcache.local" = "\\OC\\Memcache\\Redis";
+              "memcache.distributed" = "\\OC\\Memcache\\Redis";
+              "memcache.locking" = "\\OC\\Memcache\\Redis";
+
+              preview_max_x = 2048;
+              preview_max_y = 2048;
+              jpeg_quality = 60;
+            };
+          };
+
+          services.redis.servers.nextcloud = {
+            enable = true;
+            user = "nextcloud";
+            port = 0;
+          };
+
+          services.postgresql = {
+            enable = true;
+            package = pkgs.postgresql_14;
+            ensureUsers = [
+              {
+                name = "nextcloud";
+                ensureDBOwnership = true;
+              }
+            ];
+            ensureDatabases = ["nextcloud"];
+          };
+
+          systemd.services."nextcloud-setup" = {
+            requires = ["postgresql.service"];
+            after = ["postgresql.service"];
+          };
+
+          systemd.timers."nextcloud-preview-gen" = {
+            wantedBy = ["timers.target"];
+            after = ["nextcloud-setup.service"];
+            timerConfig = {
+              OnBootSec = "10m";
+              OnUnitActiveSec = "10m";
+              Unit = "nextcloud-preview-gen.service";
+            };
+          };
+
+          systemd.services."nextcloud-preview-gen" = {
+            script = ''
+              ${config.services.nextcloud.occ}/bin/nextcloud-occ preview:pre-generate
+            '';
+
+            serviceConfig = {
+              Type = "oneshot";
+              User = "root";
+            };
+          };
+
+          system.stateVersion = "22.11";
+        };
       };
     };
 }
