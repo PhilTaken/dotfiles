d636c767b5d736a21e3ebe84eeef517b354cc4a0
Philipp Herzog <ph@flyingcircus.io>
2024-06-03 12:03:40 +0200

    feat: headscale oidc

--- modules/hosts/server/headscale.nix
+++ modules/hosts/server/headscale.nix
@@ -32,6 +32,9 @@ in {
       allowedUDPPorts = [cfg.port];
       allowedTCPPorts = [cfg.port];
     };
+
+    sops.secrets."headscale-kc-client-secret".owner = config.systemd.services.headscale.serviceConfig.User;
+
     services.headscale = {
       enable = true;
       # limit to external ip on beta?
@@ -42,6 +45,13 @@ in {
         server_url = "https://${cfg.url}";
         reporting-disable = true;
         dns_config.base_domain = net.tld;
+        oidc = {
+          only_start_if_oidc_is_available = true;
+          client_secret_path = config.sops.secrets."headscale-kc-client-secret".path;
+          # TODO network.nix?
+          issuer = "https://keycloak.pherzog.xyz/realms/services";
+          client_id = "headscale";
+        };
       };
     };
     phil.server.services.caddy.proxy."${cfg.host}" = {
