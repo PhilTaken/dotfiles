12ddbee1e9e4cb38bee58d00122be90bae7099bd
Philipp Herzog <ph@flyingcircus.io>
2024-08-30 15:52:29 +0200

    mealie: remove /run/secrets patch

--- modules/hosts/server/mealie.nix
+++ modules/hosts/server/mealie.nix
@@ -32,20 +32,6 @@ in {
     services.mealie = {
       enable = true;
 
-      package =
-        pkgs.mealie.overrideAttrs
-        (old: {
-          patches =
-            (old.patches or [])
-            ++ [
-              (pkgs.fetchpatch {
-                url = "https://github.com/mealie-recipes/mealie/commit/445754c5d844ccf098f3678bc4f3cc9642bdaad6.patch";
-                hash = "sha256-ZdATmSYxhGSjoyrni+b5b8a30xQPlUeyp3VAc8OBmDY=";
-                revert = true;
-              })
-            ];
-        });
-
       listenAddress = "0.0.0.0";
       inherit (cfg) port;
 
