f45c7ea7b1d7aa1e1712028973a99afdef0b64ba
Philipp Herzog <ph@flyingcircus.io>
2024-10-07 08:52:24 +0200

    wip: disable deploys to zetta for now

--- modules/flake/deploy.nix
+++ modules/flake/deploy.nix
@@ -16,16 +16,17 @@ in {
           # all network nodes should have a headscale ip
           hostname = lib.optionalString (lib.assertMsg (nodeconfig.network_ip ? "headscale") "node ${nodename} has no headscale ip, cannot deploy") nodeconfig.network_ip."headscale";
           profiles.system.path = (activateFor nodeconfig.system) self.nixosConfigurations.${nodename};
+          sshOpts = ["-oControlMaster=no"];
         })
         net.nodes)
       // {
         # small nixos vm on mac
-        zetta = {
-          hostname = "alice";
-          sshUser = "root";
-          remoteBuild = true;
-          profiles.system.path = (activateFor "aarch64-linux") self.nixosConfigurations.zetta;
-        };
+        # zetta = {
+        #   hostname = "alice";
+        #   sshUser = "root";
+        #   remoteBuild = true;
+        #   profiles.system.path = (activateFor "aarch64-linux") self.nixosConfigurations.zetta;
+        # };
       };
   };
 }
