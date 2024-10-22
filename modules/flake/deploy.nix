887eb405f404d25e5e6bac06ae05c1192dddfdea
Philipp Herzog <ph@flyingcircus.io>
2024-10-22 11:44:24 +0200

    add backup yubikey

--- modules/flake/deploy.nix
+++ modules/flake/deploy.nix
@@ -12,6 +12,7 @@ in {
     deploy.nodes =
       (builtins.mapAttrs (nodename: nodeconfig: {
           sshUser = "nixos";
+          user = "root";
           remoteBuild = true;
           # all network nodes should have a headscale ip
           hostname = lib.optionalString (lib.assertMsg (nodeconfig.network_ip ? "headscale") "node ${nodename} has no headscale ip, cannot deploy") nodeconfig.network_ip."headscale";
