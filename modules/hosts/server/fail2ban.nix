c6dc4c921ea7b858d1c04000fa0220c399e0cc72
Philipp Herzog <ph@flyingcircus.io>
2024-10-30 09:02:06 +0100

    feat: do not fail2ban private addresses

--- modules/hosts/server/fail2ban.nix
+++ modules/hosts/server/fail2ban.nix
@@ -8,7 +8,7 @@
   cfg = config.phil.server.services.fail2ban;
 in {
   options.phil.server.services.fail2ban = {
-    enable = mkEnableOption "fail2ban ssh login blocker";
+    enable = (mkEnableOption "fail2ban ssh login blocker") // {default = true;};
   };
 
   config = mkIf cfg.enable {
@@ -16,6 +16,15 @@ in {
       fail2ban
     ];
 
-    services.fail2ban.enable = true;
+    services.fail2ban = {
+      enable = true;
+      ignoreIP =
+        ["127.0.0.0/8" "::1"]
+        ++ [
+          config.phil.network.networks.headscale.netmask
+          config.phil.network.networks.milkyway.netmask
+          config.phil.network.networks.yggdrasil.netmask
+        ];
+    };
   };
 }
