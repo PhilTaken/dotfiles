0da0f6167eb9ba989f660b40566eb1b6aeb29e49
Philipp Herzog <ph@flyingcircus.io>
2024-11-10 17:49:59 +0100

    feat: disko config for gamma

--- machines/gamma/default.nix
+++ machines/gamma/default.nix
@@ -5,81 +5,29 @@
   modulesPath,
   ...
 }: {
-  imports = [(modulesPath + "/installer/scan/not-detected.nix")];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    ./disko-config.nix
+  ];
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "cryptd" "sr_mod"];
   boot.initrd.kernelModules = ["dm-snapshot" "vfat" "nls_cp437" "nls_iso8859-1" "usbhid"];
-  boot.kernelModules = ["kvm-amd" "zfs"];
-  boot.extraModulePackages = [pkgs.zfs];
+  boot.kernelModules = ["kvm-amd"];
   boot.binfmt.emulatedSystems = ["aarch64-linux"];
   boot.loader = {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
 
-  # https://nixos.wiki/wiki/OSX-KVM
-  boot.extraModprobeConfig = ''
-    options kvm_intel nested=1
-    options kvm_intel emulate_invalid_guest_state=0
-    options kvm ignore_msrs=1
-  '';
-
   virtualisation.libvirtd.enable = true;
-
-  #boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux_latest;
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
-
-  boot.supportedFilesystems = ["zfs"];
-
-  # Enable support for the YubiKey PBA
-  boot.initrd.luks.yubikeySupport = true;
-
-  boot.initrd.luks.devices = {
-    nixos-enc = {
-      device = "/dev/disk/by-uuid/c31cf3c4-270c-46ad-b73c-c7a69acd11f1";
-      preLVM = true;
-      yubikey = {
-        slot = 2;
-        twoFactor = false;
-        storage = {
-          device = "/dev/disk/by-uuid/29B9-AA38";
-        };
-      };
-    };
-  };
-
+  boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux_latest;
   boot.plymouth.enable = true;
 
-  fileSystems = {
-    "/" = {
-      device = "rpool/root/nixos";
-      fsType = "zfs";
-    };
-
-    "/home" = {
-      device = "rpool/home";
-      fsType = "zfs";
-    };
-
-    "/boot" = {
-      device = "/dev/disk/by-uuid/29B9-AA38";
-      fsType = "vfat";
-    };
-
-    "/media/platte" = {
-      device = "/dev/disk/by-uuid/1AEA5B14EA5AEC0F";
-      fsType = "ntfs-3g";
-      options = ["defaults" "user" "rw" "utf8" "umask=000" "uid=1000" "gid=100" "exec"];
-    };
-  };
-
-  swapDevices = [
-    {
-      device = "/dev/disk/by-uuid/d00d153a-b9f1-4ebb-8803-50dc8e532fa7";
-    }
-  ];
-
   networking.hostId = "f95453f6";
+
+  disko.devices = import ./disko-config.nix {
+    disks = ["/dev/nvme0n1"];
+  };
 
   nixpkgs.overlays = [
     (_self: super: {
@@ -118,11 +66,6 @@
               {
                 criteria = "Philips Consumer Electronics Company PHL 245E1 0x000072DC";
                 mode = "2560x1440@74.968002";
-                position = "0,1080";
-              }
-              {
-                criteria = "WOR TERRA 2455W W507LSD00315";
-                mode = "1920x1080@60.000000";
                 position = "0,0";
               }
             ];
---
+++ machines/gamma/disko-config.nix
@@ -0,0 +1,32 @@
+{disks ? ["/dev/nvme0n1"], ...}: {
+  disk = {
+    vdb = {
+      device = builtins.elemAt disks 0;
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            end = "500M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = ["umask=0077"];
+            };
+          };
+          root = {
+            name = "root";
+            end = "-0";
+            content = {
+              type = "filesystem";
+              format = "bcachefs";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
+    };
+  };
+}
