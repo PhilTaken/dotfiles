cc0099b69cc08cb2cfa24caac3b03b6cf4b7110d
Philipp Herzog <ph@flyingcircus.io>
2024-12-08 01:13:25 +0100

    feat: samba share on delta

--- machines/delta/default.nix
+++ machines/delta/default.nix
@@ -24,4 +24,41 @@
       libvdpau-va-gl
     ];
   };
+
+  # TODO move to fileshare?
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    openFirewall = true;
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "smbnix";
+        "netbios name" = "smbnix";
+        "security" = "user";
+        #"use sendfile" = "yes";
+        #"max protocol" = "smb2";
+        # note: localhost is the ipv6 localhost ::1
+        "hosts allow" = "192.168.0. 192.168.178.0. 10.64.0.0. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };
+      "public" = {
+        "path" = "/media/mount/samba";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "username";
+        "force group" = "groupname";
+      };
+    };
+  };
+
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
 }
