3fcc0880120a737ffc237f4af225b0b5f564ca0d
Philipp Herzog <philipp.herzog@protonmail.com>
2025-01-04 23:36:33 +0100

    fix: remove old flameshot config

--- modules/users/wms/i3/default.nix
+++ modules/users/wms/i3/default.nix
@@ -185,23 +185,6 @@ in {
       package = pkgs.pulseeffects-legacy;
     };
 
-    systemd.user.services.flameshot = {
-      Unit = {
-        Description = "Unit for the flameshot daemon";
-        After = "graphical-session-pre.target";
-        PartOf = "graphical-session.target";
-      };
-
-      Service = {
-        ExecStart = "${pkgs.flameshot}/bin/flameshot";
-        Restart = "on-abort";
-      };
-
-      Install = {
-        WantedBy = ["graphical-session.target"];
-      };
-    };
-
     home.packages = with pkgs; [
       flameshot
       feh
